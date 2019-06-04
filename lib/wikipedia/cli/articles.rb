eb42e3f37a6c8b4fbd219d539122314dc1942c5c
Solomon Raphael Feldman <SolomonRFeldman@gmail.com>
2019-06-04 16:34:45 -0400

    first commit

---
+++ lib/wikipedia/cli/articles.rb
@@ -0,0 +1,10 @@
+require "wikipedia/cli/articles/version"
+
+module Wikipedia
+  module Cli
+    module Articles
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/wikipedia/cli/articles/version.rb
@@ -0,0 +1,7 @@
+module Wikipedia
+  module Cli
+    module Articles
+      VERSION = "0.1.0"
+    end
+  end
+end
