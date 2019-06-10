3d114f7b94ce3b12fdeff4ca74bcc8f098e911aa
Solomon Raphael Feldman <SolomonRFeldman@gmail.com>
2019-06-10 16:21:28 -0400

    added environment

---
+++ lib/wikipedia_cli_articles.rb
@@ -0,0 +1,6 @@
+require 'pry'
+
+require_relative 'wikipedia_cli_articles/command_line_interface'
+require_relative 'wikipedia_cli_articles/page'
+require_relative 'wikipedia_cli_articles/scraper'
+require_relative 'wikipedia_cli_articles/section'
