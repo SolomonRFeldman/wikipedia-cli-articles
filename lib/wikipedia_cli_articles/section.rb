c8f825a83c76a061d61f167164c04a4f6d89a648
Solomon Raphael Feldman <SolomonRFeldman@gmail.com>
2019-06-11 17:13:50 -0400

    paragraphs array to sections

--- lib/wikipedia_cli_articles/section.rb
+++ lib/wikipedia_cli_articles/section.rb
@@ -1,5 +1,9 @@
 #Will have a section name to identify which section the user wants to read, and it will have a body text
 class Section
-  attr_accessor :title
+  attr_accessor :title, :paragraphs
+
+  def initialize
+    @paragraphs = []
+  end
 
 end
